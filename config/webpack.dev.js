7b2655d28d0213fb36d3c8f0509c6c8a79907901
J S <jost2006@googlemail.com>
2017-03-15 17:33:12 +0100

    var

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -51,7 +51,8 @@ module.exports = function (options) {
      * See: http://webpack.github.io/docs/configuration.html#devtool
      * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
      */
-    devtool: 'cheap-module-source-map',
+/*    devtool: 'cheap-module-source-map',
+*/    devtool: 'source-map',
 
     /**
      * Options affecting the output of the compilation.
